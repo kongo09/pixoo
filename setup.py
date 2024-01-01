fc684ada3c796927898277f3e8e5cddebcc2e524
kongo09 <kongo09@users.noreply.github.com>
2024-01-01 15:30:26 +0100

    provide readme for pypi

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@
 """
     Setup.py file for pixoo package
 """
+from pathlib import Path
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+longdescription = (this_directory / "README.md").read_text()
 
 setup(
     name="pixoo",
-    version="0.8.0",
+    version="0.8.1",
     author="Ron Talman, kongo09",
     description=(
         "A library to easily communicate with the Divoom Pixoo 64",
         "(and hopefully soon more screens that support Wi-Fi)",
     ),
+    long_description=longdescription,
+    long_description_content_type='text/markdown',
     license="BSD",
     keywords="pixoo",
     url="https://github.com/kongo09/pixoo#readme",
@@ -24,5 +30,6 @@ setup(
         'Pillow ~= 10.0.0',
     ],
     python_requires=">=3.10",
-    package_dir={"": "."},
+#    package_dir={"": "."},
+    include_package_data=True,
 )
